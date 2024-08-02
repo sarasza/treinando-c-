7f4ec40d0217c4a6df67e76f2f6ef96ac93194c5
Sara Souza <105318470+sarasza@users.noreply.github.com>
2024-08-02 14:59:46 -0300

    Create precoMetroQuadrado2.cs

---
+++ precoMetroQuadrado2.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width, length, area, pricePerSquareMeter, priceTotal;
+
+            Console.WriteLine("Digite a largura: ");
+            width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o comprimento: ");
+            length = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o preço por metro quadrado: ");
+            pricePerSquareMeter = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = width * length;
+            priceTotal = area * pricePerSquareMeter;
+
+            Console.WriteLine("A área total é: " + area.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("O valor do terreno é: " + priceTotal.ToString("F2"), CultureInfo.InvariantCulture) ;
+
+            Console.ReadLine();
+        }
+    }
+}
